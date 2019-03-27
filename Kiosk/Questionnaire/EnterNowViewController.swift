0947fdf170d9abf9a47fb8ecd17b7a04031ef6c7
mohinimehetre <mohinimehetre@gmail.com>
2019-03-27 20:20:56 +0530

    MM: Added temparory french text
    MM: Added temparory french text

--- Kiosk/Questionnaire/EnterNowViewController.swift
+++ Kiosk/Questionnaire/EnterNowViewController.swift
@@ -9,22 +9,43 @@
 import UIKit
 
 class EnterNowViewController: UIViewController {
-
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        self.initializeView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Methods
+    func initializeView() {
+        let isFrenchSelected = true
+        if (isFrenchSelected) {
+            let frenchString: String = "COMMENCEZ MAINTENANT POUR OBTENIR VOTRE PRODUIT GRATUIT MILWAUKEE"
+            let attributeText_french = NSMutableAttributedString.init(string: frenchString)
+            
+            frenchString.enumerateSubstrings(in: frenchString.startIndex..<frenchString.endIndex, options: .byWords) {
+                (substring, substringRange, _, _) in
+                if substring == "MILWAUKEE" {
+                    attributeText_french.addAttribute(.foregroundColor, value: UIColor.black,
+                                                      range: NSRange(substringRange, in: frenchString))
+                }
+            }
+            infoLabel.attributedText = attributeText_french
+        }
     }
-    */
-
+    
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
