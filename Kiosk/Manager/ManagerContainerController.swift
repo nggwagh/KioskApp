e34b28fde2d8ec70c298fcc6cf83b24d865169b6
MohiniMehetre <mohinimehetre@gmail.com>
2019-03-30 13:28:41 +0530

    MM: Added files related to manager
    MM: Added files related to manager

---
+++ Kiosk/Manager/ManagerContainerController.swift
@@ -0,0 +1,30 @@
+//
+//  ManagerContainerController.swift
+//  Kiosk
+//
+//  Created by Mohini Mehetre on 30/03/19.
+//  Copyright © 2019 Mayur Deshmukh. All rights reserved.
+//
+
+import UIKit
+
+class ManagerContainerController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
