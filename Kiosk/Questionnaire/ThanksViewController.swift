6cdf69e01b3e734fcf0782e6181d727d134cbc08
Nikhil Wagh <nikhil.g.wagh@gmail.com>
2019-04-16 00:05:15 +0530

    released 1.0(7) version with thank you screen text change

--- Kiosk/Questionnaire/ThanksViewController.swift
+++ Kiosk/Questionnaire/ThanksViewController.swift
@@ -26,8 +26,10 @@ class ThanksViewController: UIViewController {
     // MARK: - Private Methods
     func initializeView() {
         let languagePreference = UserDefaults.standard.value(forKey: Constant.UserDefaultKey.languagePreference) as! String
-        if (languagePreference == "FR") {
-            let frenchString: String = "MERCI \n SIIL VOUS PLATT VOIR LE REPRÉSENTANT DE MILWAUKEE POUR RÉCLAMER VOTRE BON"
+       if (languagePreference == "FR") {
+//            let frenchString: String = "MERCI \n SIIL VOUS PLATT VOIR LE REPRÉSENTANT DE MILWAUKEE POUR RÉCLAMER VOTRE BON"
+            let frenchString: String = "JE VOUS REMERCIE \n VEUILLEZ CONSULTER LE REPRÉSENTANT DE MILWAUKEE POUR RÉCLAMER VOTRE BIEN GRATUIT"
+
             let attributeText_french = NSMutableAttributedString.init(string: frenchString)
             attributeText_french.addAttribute(.foregroundColor, value: UIColor.red,
                                               range: NSMakeRange(0, attributeText_french.length))
@@ -35,7 +37,7 @@ class ThanksViewController: UIViewController {
 
             frenchString.enumerateSubstrings(in: frenchString.startIndex..<frenchString.endIndex, options: .byWords) {
                 (substring, substringRange, _, _) in
-                if substring == "MERCI" {
+                if (substring == "JE" || substring == "VOUS" || substring == "REMERCIE"){
                     attributeText_french.addAttribute(.foregroundColor, value: UIColor.black,
                                                       range: NSRange(substringRange, in: frenchString))
                     
@@ -45,6 +47,7 @@ class ThanksViewController: UIViewController {
             infoLabel.attributedText = attributeText_french
             finishedButton.setTitle("FINI", for: UIControlState.normal)
 
+        
         }
         else {
             let englishString: String = "THANK \n YOU \n PLEASE SEE THE \n MILWAUKEE REPRESENTATIVE TO CLAIM YOUR GOOD"
@@ -66,6 +69,7 @@ class ThanksViewController: UIViewController {
             finishedButton.setTitle("FINISHED", for: UIControlState.normal)
 
         }
+   
     }
     
     @objc func navigateToEnterNowScreen() {
